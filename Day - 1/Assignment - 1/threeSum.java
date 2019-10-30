03c65c0f6e945fe9d724f75c7e80df3f94194309
Nikhil-RT <nraviteja403@msitprogram.net>
2019-10-30 14:46:35 +0530

    done!

--- Day - 1/Assignment - 1/threeSum.java
+++ Day - 1/Assignment - 1/threeSum.java
@@ -2,34 +2,39 @@ import java.util.Arrays;
 
 public class threeSum {
     public static void main(String[] args) {
-        int [] a = {1,2,-3,4,-5,6,7,-13};
+        int [] a = {20,-1,-19,-16,4,12};
         int N = a.length;
         Arrays.sort(a);
         int count = 0;
         int high  = a.length-1;
         int low = 0;
         int medium;
-        for (int i = 0; i < N; i++){
-            for (int j = i + 1; i < N; j ++){
+
+        for (int i = 0; i < N; i++) {
+
+            for (int j = i + 1; j < N; j ++) {
+
                 high = a.length - 1;
                 low = 0;
+                
                 while(low <= high){
                     medium = (high + low)/2;
-                    if(a[medium]==a[i]+a[j]){
-                        if(a[low] < a[medium] && a[medium] < a[high]) {
+
+                    if( -a[medium] == a[i]+a[j] ) {
+
+                        if(a[low] < a[medium] && a[medium] < a[high]) 
                             count++;
                             break;
-                        }
-                        else if ((a[medium]) < (a[i]+a[j])*-1){
-                            low = medium + 1;
-                        }
-                        else{
-                            high = medium - 1;
-                        }
+
+                    }else if (a[medium] < (a[i]+a[j])*-1) {
+                        low = medium + 1;
+                    }
+                    else {
+                       high = medium - 1;
                     }
                 }
             }
         }
-    System.out.println(count);    
+         System.out.println(count);    
     }   
 }
