66006cb2f445ec69f36e1d7f0a2d544f9dd2cc9d
Nikhil-RT <nraviteja403@msitprogram.net>
2019-11-05 10:15:05 +0530

    done!

--- Day - 6/Assignment - 1/Solution.java
+++ Day - 6/Assignment - 1/Solution.java
@@ -5,16 +5,20 @@ class Solution {
 			return false;
 		}
 		
-		for ( int i = 1; i <= arr.length; i++) {
-			
+		for ( int i = 0; i <= arr.length; i++) {
+			// System.out.println(arr[i]);
 			// if( arr[0] <= arr.length-1 || arr[0] <= 0) {
-				if(2*i < arr.length && 2*i+1 < arr.length) {
-					
-					if( arr[i] > arr[2*i]  && arr[i] > arr[2*i+1] ) {
+				if(2*i+1 < arr.length && 2*i+2 < arr.length) {
+
+					if( arr[i] > arr[2*i+1]  && arr[i] > arr[2*i+2] ) {
 						return false;
 					}	
 			}
 		}
 		return true;
+	}
+	public static void main(String[] args) {
+		double[] arr = {121,125,123,126,127,124};
+		System.out.println(isMinHeap(arr));
 	}
 }
