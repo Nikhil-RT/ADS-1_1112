1ac3f42275a3edf8c8e33f9e21f90562687d5ba4
Nikhil-RT <nraviteja403@msitprogram.net>
2019-11-05 14:11:16 +0530

    Updated with checkstyles !

--- Day - 6/Assignment - 1/Solution.java
+++ Day - 6/Assignment - 1/Solution.java
@@ -1,24 +1,18 @@
-class Solution {
-	
-	public static boolean isMinHeap(double[] arr) {
-		if(arr.length == 0){
-			return false;
-		}
-		
-		for ( int i = 0; i <= arr.length; i++) {
-			// System.out.println(arr[i]);
-			// if( arr[0] <= arr.length-1 || arr[0] <= 0) {
-				if(2*i+1 < arr.length && 2*i+2 < arr.length) {
-
-					if( arr[i] > arr[2*i+1]  && arr[i] > arr[2*i+2] ) {
-						return false;
-					}	
-			}
-		}
-		return true;
-	}
-	public static void main(String[] args) {
-		double[] arr = {121,125,123,126,127,124};
-		System.out.println(isMinHeap(arr));
-	}
+final class Solution {
+    private Solution() { }
+    public static boolean isMinHeap(final double[] arr) {
+        int a = arr.length;
+        if (a == 0) {
+            return false;
+        }
+        for (int i = 0; i <= a; i++) {
+                if (2 * i + 1 < a && 2 * i + 2 < a) {
+                    if (arr[i] > arr[2 * i + 1]
+                    || arr[i] > arr[2 * i + 2]) {
+                        return false;
+                    }
+            }
+        }
+        return true;
+    }
 }
