673f8294139de5b7ec3735135735c37fe02a690d
Nikhil-RT <nraviteja403@msitprogram.net>
2019-11-14 19:05:29 +0530

    Done!

--- New Assignments/Assignment - 1/FarthestPair.java
+++ New Assignments/Assignment - 1/FarthestPair.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 /**
  * Write a program that, given an array a[] of N double values, 
@@ -12,19 +13,32 @@ class FarthestPair{
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter No. of values: ");
-
         int number = scan.nextInt();
+        // We do try & catch operations for errors
+        do{
+        try{    
+        if(number < 0) {
+            throw new InputMismatchException();
+        }
+        }
+        catch(InputMismatchException exception) {
+            System.out.println("Enter the Input again");
+            //Again call for input
+            number = scan.nextInt();
+        }
+        }while(false);
         double[] a = new double[number];
         
         // Let's take an Double Array
         // double a[] = {5.0, 7.0, 2.0, 4.0, 3.0}; 
         int i = 0;
-        //Iterating using While loop
+        //Iterating input using for loop
         for (int j = 0; j < a.length; j++) {
             a[j] = scan.nextDouble();
         }
         double Min = a[0];
         double Max = a[0];
+        //Iterating using While loop
         while (i < a.length) { 
             //Finding Min
             if(a[i] < Min) {
@@ -41,7 +55,6 @@ class FarthestPair{
         //Printing Min and Max in a pair
         System.out.println("(" + Min + "," + Max + ")");
         // System.out.println();
-
 
     }
 }
