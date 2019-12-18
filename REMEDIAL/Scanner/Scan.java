f9243e47cfbef656352832733177470d65befc27
Nikhil-RT <nraviteja403@msitprogram.net>
2019-12-18 16:45:30 +0530

    Done!

--- REMEDIAL/Scanner/Scan.java
+++ REMEDIAL/Scanner/Scan.java
@@ -1,6 +1,26 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 import org.omg.CORBA.INTERNAL;
+
+class Student {
+    String name;
+    int maths;
+    int science;
+    int social;
+    
+    public Student(String name, int maths, int science, int social) {
+        this.name = name;
+        this.maths = maths;
+        this.science = science;
+        this.social = social;
+    }
+
+    public String toString() {
+        return this.name +" "+this.maths +" "+ this.science +" "+ this.social;
+    }
+
+}
 
 class Scan {
     /**
@@ -14,16 +34,27 @@ class Scan {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        String in = scan.nextLine();
-        int in1 = scan.nextInt();
-        int in2 = scan.nextInt();
-        double in3 = scan.nextDouble();
-        String in4 = scan.next();
-        System.out.println(in);
-        System.out.println(in1);
-        System.out.println(in2);
-        System.out.println(in3);
-        System.out.println(in4);
+        System.out.println("Enter no.of lines : ");
+        String noofLines = scan.nextLine();
+        int n = Integer.parseInt(noofLines);
+        // String[] StudentArr = new String[n];
+        Student[] arr = new Student[n];
+
+        for (int i = 0; i < n;  i++) {
+            String name = scan.next();
+            int maths = scan.nextInt();
+            int science = scan.nextInt();
+            int social = scan.nextInt();
+            // String s = scan.nextLine();
+            Student a = new Student(name, maths, science, social);
+            arr[i] = a;
+            // System.out.println(arr[i]);
+        }
+        System.out.println("=========THE OUTPUT=========");
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(Arrays.toString(arr));
         scan.close();
     }
 }
